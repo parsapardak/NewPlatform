d6ae9de3ee57f671099f38e4fbd7b36cc118595e
parsapardak <parsa.pardak@gmail.com>
2025-01-12 01:16:00 +0330

    TAMOOOOOOOOM

--- comments/urls.py
+++ comments/urls.py
@@ -4,7 +4,6 @@ from .views import add_comment, add_reply, edit_comment, delete_comment
 urlpatterns = [
     path('<int:news_id>/add/', add_comment, name='add-comment'),  # ارسال نظر
     path('<int:comment_id>/reply/', add_reply, name='add-reply'),  # پاسخ به نظر
-    path('<int:comment_id>/edit/', edit_comment, name='edit-comment'),
-    path('<int:comment_id>/delete/', delete_comment, name='delete-comment'),
-
+    path('<int:comment_id>/edit/', edit_comment, name='edit-comment'),  # ویرایش نظر
+    path('<int:comment_id>/delete/', delete_comment, name='delete-comment'),  # حذف نظر
 ]
